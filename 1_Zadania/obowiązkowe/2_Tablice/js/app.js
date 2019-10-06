ff2496a700bb558f855ce148f85adad66a932e9f
Marcin Migdał <marcin.migdal@interia.pl>
2019-10-06 17:22:05 +0200

    Update app.js

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -17,3 +17,13 @@ function distFromAverage(array) {
 
 distFromAverage([1, 2, 3, 4, 5, 6, 7]);
 
+var array = ['apple', 'bannana', 'pear', 'orange'];
+
+console.log(array[0]);
+
+console.log(array[array.length - 1]);
+
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
+
